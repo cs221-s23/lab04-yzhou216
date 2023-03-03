9954d3a81ff4ab17100991fef01e06c160d10ec1
Yiyu Zhou <yzhou155@vlab00.cs.usfca.edu>
2023-03-02 23:05:25 -0800

    Clean up

--- test.c
+++ test.c
@@ -21,19 +21,19 @@ int main(int argc, char **argv)
 
 	board_t b;
 
-	if (strcmp(argv[1], "empty") == 0) {
-		char* argv[] = {"_", "_", "_", "_", "_", "_", "_", "_", "_"};
+	if (!strcmp(argv[1], "empty")) {
+		char *argv[] = {"_", "_", "_", "_", "_", "_", "_", "_", "_"};
 		init_board(b, argv);
 		print_board(b);
-	} else if (strcmp(argv[1], "print") == 0) {
+	} else if (!strcmp(argv[1], "print")) {
 		for (int r = 0; r < BOARD_SZ; r++) {
 			for (int c = 0; c < BOARD_SZ; c++) {
 				b[r][c] = '_';
 			}
 		}
 		print_board(b);
-	} else if (strcmp(argv[1], "init") == 0) {
-		char* argv[]  = {"X", "_", "O", "_", "X", "_", "_", "_", "O"};
+	} else if (!strcmp(argv[1], "init")) {
+		char *argv[]  = {"X", "_", "O", "_", "X", "_", "_", "_", "O"};
 		init_board(b, argv);
 		if ((b[0][0] == 'X') && (b[0][0] == b[1][1]) &&
 		    (b[0][1] == '_') && (b[0][1] == b[1][0]) &&
@@ -44,37 +44,37 @@ int main(int argc, char **argv)
 		} else {
 			printf("init fail\n");
 		}
-	} else if (strcmp(argv[1], "board") == 0) {
-		char* argv[] = {"X", "_", "O", "_", "X", "_", "_", "_", "O"};
+	} else if (!strcmp(argv[1], "board")) {
+		char *argv[] = {"X", "_", "O", "_", "X", "_", "_", "_", "O"};
 		init_board(b, argv);
 		print_board(b);
-	} else if (strcmp(argv[1], "check") == 0) {
+	} else if (!strcmp(argv[1], "check")) {
 		b[0][0] = b[0][1] = b[0][2] = 'X';
 		b[1][0] = b[1][2] = b[2][0] = b[2][1] = '_';
 		b[1][1] = b[2][2] = 'O';
 		print_result(check_board(b));
-	} else if (strcmp(argv[1], "xhoriz") == 0) {
-		char* argv[]  = {"X", "X", "X", "_", "O", "_", "_", "_", "O"};
+	} else if (!strcmp(argv[1], "xhoriz")) {
+		char *argv[]  = {"X", "X", "X", "_", "O", "_", "_", "_", "O"};
 		init_board(b, argv);
 		print_board(b);
 		print_result(check_board(b));
-	} else if (strcmp(argv[1], "overt") == 0) {
-		char* argv[]  = { "X", "X", "O", "_", "_", "O", "_", "_", "O"};
+	} else if (!strcmp(argv[1], "overt")) {
+		char *argv[]  = { "X", "X", "O", "_", "_", "O", "_", "_", "O"};
 		init_board(b, argv);
 		print_board(b);
 		print_result(check_board(b));
-	} else if (strcmp(argv[1], "diag1") == 0) {
-		char* argv[]  = {"O", "X", "X", "_", "O", "X", "X", "_", "O"};
+	} else if (!strcmp(argv[1], "diag1")) {
+		char *argv[]  = {"O", "X", "X", "_", "O", "X", "X", "_", "O"};
 		init_board(b, argv);
 		print_board(b);
 		print_result(check_board(b));
-	} else if (strcmp(argv[1], "diag2") == 0) {
-		char* argv[]  = {"O", "X", "X", "_", "X", "X", "X", "_", "O"};
+	} else if (!strcmp(argv[1], "diag2")) {
+		char *argv[]  = {"O", "X", "X", "_", "X", "X", "X", "_", "O"};
 		init_board(b, argv);
 		print_board(b);
 		print_result(check_board(b));
-	} else if (strcmp(argv[1], "draw") == 0) {
-		char* argv[]  = {"X", "O", "X", "O", "X", "O", "O", "X", "O"};
+	} else if (!strcmp(argv[1], "draw")) {
+		char *argv[]  = {"X", "O", "X", "O", "X", "O", "O", "X", "O"};
 		init_board(b, argv);
 		print_board(b);
 		print_result(check_board(b));
